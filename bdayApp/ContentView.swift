daec0528e4f38a86ad3b41c2394ddea81ebb68b4
Ruth Mehreteab <ruthmehreteab12@gmail.com>
2025-07-11 10:11:36 -0400

    Initial Commit

---
+++ bdayApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  bdayApp
+//
+//  Created by Ruth Mehreteab on 7/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ bdayApp/bdayAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  bdayAppApp.swift
+//  bdayApp
+//
+//  Created by Ruth Mehreteab on 7/11/25.
+//
+
+import SwiftUI
+
+@main
+struct bdayAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
