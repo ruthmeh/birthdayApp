ed936198035b7b9f30d2561ead6ed91b12a21395
Ruth Mehreteab <ruthmehreteab12@gmail.com>
2025-07-11 11:34:23 -0400

    set up my UI

---
+++ Friend.swift
@@ -0,0 +1,18 @@
+//
+//  Friend.swift
+//  bdayApp
+//
+//  Created by Ruth Mehreteab on 7/11/25.
+//
+
+import Foundation
+
+class Friend{
+    var name: String
+    var birthday: Date
+    
+    init(name: String, birthday: Date) {
+        self.name = name
+        self.birthday = birthday
+    }
+}
--- bdayApp/ContentView.swift
+++ bdayApp/ContentView.swift
@@ -8,15 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var newName = ""
+    @State private var newBirthday = Date.now
+    
+    // friend array
+    @State private var friends: [Friend] = [
+        Friend(name: "Ruth", birthday: .now),
+        Friend(name: "Helen", birthday: .now)
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            List(friends, id: \.name){ friend in
+                HStack{
+                    Text(friend.name)
+                    Spacer()
+                    Text(friend.birthday, format: .dateTime.month(.wide).day().year())
+                }
+            }
+            .navigationTitle("Birthdays")
+            .safeAreaInset(edge: .bottom)
+            {
+                VStack(alignment: .center, spacing: 20)
+                {
+                    Text("New Birthday")
+                        .font(.headline)
+                    DatePicker(selection: $newBirthday,
+                            in:
+                            Date.distantPast...Date.now,
+                            displayedComponents: .date){
+                        TextField("Name", text: $newName)
+                            .textFieldStyle(.roundedBorder)
+                    }
+                    Button("Save"){
+                        let newFriend = Friend(name: newName, birthday: newBirthday)
+                        friends.append(newFriend)
+                        newName = ""
+                        newBirthday = .now
+                    }
+                    .bold()
+                }
+                .padding()
+                    .background(.bar)
+            }
+            // closing nav stack
         }
-        .padding()
+        // closing the body
     }
+    // closing the structure
+    
 }
 
 #Preview {
